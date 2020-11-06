b8da76e715ce6578997d362dcef939e86a127a4b
quapik <73609809+quapik@users.noreply.github.com>
2020-11-06 22:28:27 +0100

    Update symtable.c

--- symtable.c
+++ symtable.c
@@ -7,24 +7,182 @@ Fabián Michal   xfabia13@stud.fit.vutbr.cz
 Poposki Vasil   xpopos00@stud.fit.vutbr.cz
 Prosinec 2020, Fakulta informačních technologií VUT v Brně
 */
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+
+//tabulka symbolu
+#include "symtable.h"
+//BST(BinarySearchTree - viz IAL projekt2, c401)
+//Inicializace stromu
+void BSTInit (tBSTNodePtr *RootPtr) {
+
+*(RootPtr)=NULL;
+}
+//Vyhledavani v stromu
+int BSTSearch (tBSTNodePtr RootPtr, char K, int *Content)	{
+
+if(RootPtr==NULL)
+	{
+		return(FALSE);
+	}
+else
+	{
+		if(RootPtr->Key==K)
+			{
+				(*Content)=RootPtr->BSTNodeCont;
+				return(TRUE); //pokud je rootKey roven tak koncime a dal nehledame
+			}
+		else
+			{
+				if(RootPtr->Key>K)
+					{
+						return BSTSearch(RootPtr->LPtr,K,Content); //rekurzivnie prohledavani levyho podstromu
+					}
+					else
+						{
+						return BSTSearch(RootPtr->RPtr,K,Content);	//rekurzivnie prohledavani pravyho podstromu	
+						}
+			}
+
+	}	
+
+}
+//Vlozeni do stromu
+void BSTInsert (tBSTNodePtr* RootPtr, char K, int Content)	{
+	tBSTNodePtr polozka;
+	if((*RootPtr)==NULL) //pokud je prazdno
+	{
+		polozka=(tBSTNodePtr) malloc(sizeof(struct tBSTNode));
+		if(polozka==NULL) //malloc se nepovedl
+		{
+			return;
+		}
+		else
+		{
+			polozka->Key=K;
+			polozka->BSTNodeCont=Content;
+			polozka->RPtr=NULL; //konečný list
+			polozka->LPtr=NULL;
+			*(RootPtr)=polozka;
+		} 
+
+	}	
+	else
+	{
+		if((*RootPtr)->Key>K)
+		{
+			BSTInsert(&((*RootPtr)->LPtr),K,Content); //doleva a rekuzivne jedeme dal
+		}
+		else
+		{
+			if((*RootPtr)->Key<K)
+			{
+				BSTInsert(&((*RootPtr)->RPtr),K,Content); //doprava
+			}
+
+			else
+			{
+				(*RootPtr)->BSTNodeCont=Content; //pokud jiz existuje tak je nahrazen
+			}
+			
+		}
+	}
+}   
+//Nahrazeni nejpravejsim
+void ReplaceByRightmost (tBSTNodePtr PtrReplaced, tBSTNodePtr *RootPtr){
+if((*RootPtr)==NULL)
+	{
+		return;
+	}
+	else
+	{
+	tBSTNodePtr polozka;
+	if((*RootPtr)->RPtr==NULL) //dalsi pravy neexistuje
+	{
+			//presun nejpravejsi polozky do PtrReplaced
+		PtrReplaced->Key=(*RootPtr)->Key;
+		PtrReplaced->BSTNodeCont=(*RootPtr)->BSTNodeCont;
+		polozka=(*RootPtr);	
+		(*RootPtr)=(*RootPtr)->LPtr; //posun rootu na levy 
+		free(polozka); 
+	}
+	else //stale musime rekurzivne hledat pravej
+	{
+		ReplaceByRightmost(PtrReplaced,&(*RootPtr)->RPtr);
+	}
+
+	}
+}
+//Odstraneni uzlu podle klice
+void BSTDelete (tBSTNodePtr *RootPtr, char K) 		{
+    if((*RootPtr)==NULL)
+	{
+			return;
+	}
+	else
+	{
+		tBSTNodePtr polozka;
+		if((*RootPtr)->Key>K) //ruseny v levem podstromu
+		{
+			 BSTDelete(&(*RootPtr)->LPtr,K); //rekurzivni volani deletu (dal se hleda K)
+		}
+
+		else
+
+		{
+			if((*RootPtr)->Key<K) //ruseny v pravme podstrmu
+			{
+				BSTDelete(&(*RootPtr)->RPtr,K);
+			}
 
 
+			else 
+			{
+				if((*RootPtr)->Key==K) //nalezen uzel s danym klicem
+				{
+					if((*RootPtr)->RPtr!=NULL&&((*RootPtr)->LPtr!=NULL)) //ruseny ma oba podstromy
+					{
+						ReplaceByRightmost((*RootPtr),&(*RootPtr)->LPtr); //nahrada nejpravejsim
+					}
 
-/* uzel stromu */
-typedef struct tBSTNode {
-	char Key;			                                                      /* klíč */
-	int BSTNodeCont;                                            /* užitečný obsah uzlu */
-	struct tBSTNode * LPtr;                                    /* levý podstrom */
-	struct tBSTNode * RPtr;                                   /* pravý podstrom */
-} *tBSTNodePtr;
+					else 
+					{	if((*RootPtr)->RPtr==NULL) //nema pravy podstrom (ale ma levy)
+						{
+							polozka=(*RootPtr);
+							(*RootPtr)=(*RootPtr)->LPtr; //premosteni rootu na leveho syna
+							free(polozka);
 
-/* prototypy funkcí */
+						}
+						else
+						{
+							if (((*RootPtr)->LPtr==NULL)) //nema levy podstrom
+							{
+								polozka=(*RootPtr);
+								(*RootPtr)=(*RootPtr)->RPtr; //premosteni rootu na praveho syna
+								free(polozka);
 
-void BSTInit   (tBSTNodePtr *);
-int BSTSearch  (tBSTNodePtr, char, int *);
-void BSTInsert (tBSTNodePtr *, char, int);
-void BSTDelete (tBSTNodePtr *, char);
-void BSTDispose(tBSTNodePtr *);
+							}
+						}
+					}
+				}		
+			}
+
+		}
+	
+	}
+}
+//Zruseni stromu
+void BSTDispose (tBSTNodePtr *RootPtr) {
+ 
+	 if((*RootPtr)==NULL)
+	 	{
+			 return; //ukonceni rekurze
+	 	}	
+	else
+		{
+
+			BSTDispose(&(*RootPtr)->LPtr); //rekuze, porad vice do hloubky stromu
+			BSTDispose(&(*RootPtr)->RPtr);
+			free(*RootPtr);
+			(*RootPtr)=NULL; //uvolneni rootu(korene stromu) a navraceni do puvodniho stavu
+		}		 
+
+}  
