f4f706359b23265507332e6d85593a3534b534f3
quapik <73609809+quapik@users.noreply.github.com>
2020-11-06 22:19:36 +0100

    Update symtable.h

--- symtable.h
+++ symtable.h
@@ -1,1 +1,24 @@
 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+
+/* uzel stromu */
+typedef struct tBSTNode {
+	char Key;			                                                      /* klíč */
+	int BSTNodeCont;                                            /* užitečný obsah uzlu */
+	struct tBSTNode * LPtr;                                    /* levý podstrom */
+	struct tBSTNode * RPtr;                                   /* pravý podstrom */
+} *tBSTNodePtr;
+
+/* prototypy funkcí */
+
+void BSTInit   (tBSTNodePtr *);
+int BSTSearch  (tBSTNodePtr, char, int *);
+void BSTInsert (tBSTNodePtr *, char, int);
+void BSTDelete (tBSTNodePtr *, char);
+void BSTDispose(tBSTNodePtr *);
+
