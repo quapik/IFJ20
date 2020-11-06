0b5313e3fea12fd2fd70b7d484b375719508b2c7
quapik <73609809+quapik@users.noreply.github.com>
2020-11-06 22:28:45 +0100

    Update symtable.h

--- symtable.h
+++ symtable.h
@@ -1,5 +1,12 @@
-
-
+//Implementace překladače imperativního jazyka IFJ20
+//Tým číslo 041, varianta I
+/*Autoři projektu:
+Šíma Vojtěch 	xsimav01@stud.fit.vutbr.cz
+Fabián Michal   xfabia13@stud.fit.vutbr.cz
+Čábela Radek    xcabel04@stud.fit.vutbr.cz
+Poposki Vasil   xpopos00@stud.fit.vutbr.cz
+Prosinec 2020, Fakulta informačních technologií VUT v Brně
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
@@ -21,4 +28,3 @@ int BSTSearch  (tBSTNodePtr, char, int *);
 void BSTInsert (tBSTNodePtr *, char, int);
 void BSTDelete (tBSTNodePtr *, char);
 void BSTDispose(tBSTNodePtr *);
-
