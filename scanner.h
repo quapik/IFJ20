559d278d3e76b108248d3d1da9697d8f2f6be75d
RadekCab <myradek2@seznam.cz>
2020-11-07 16:36:49 +0100

    Create scanner.h

---
+++ scanner.h
@@ -0,0 +1,28 @@
+//Implementace překladače imperativního jazyka IFJ20
+//Tým číslo 041, varianta I
+/*Autoři projektu:
+Šíma Vojtěch 	xsimav01@stud.fit.vutbr.cz
+Fabián Michal   xfabia13@stud.fit.vutbr.cz
+Čábela Radek    xcabel04@stud.fit.vutbr.cz
+Poposki Vasil   xpopos00@stud.fit.vutbr.cz
+Prosinec 2020, Fakulta informačních technologií VUT v Brně
+
+Lexikalni Analyzator
+*/
+
+#pragma once
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+/* Stavy FSM Lexikalniho analyzatoru
+ * Nazvy odpovidaji grafu */
+
+ typedef enum{
+
+ } lState;
+
