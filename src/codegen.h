d1ec38bb5b8fb1921b0a5916e5282b7a96813ab3
quapik <73609809+quapik@users.noreply.github.com>
2020-11-29 21:29:36 +0100

    Update codegen.h

--- src/codegen.h
+++ src/codegen.h
@@ -1,1 +1,15 @@
-
+//Implementace překladače imperativního jazyka IFJ20
+//Tým číslo 041, varianta I
+/*Autoři projektu:
+Šíma Vojtěch 	xsimav01@stud.fit.vutbr.cz
+Fabián Michal   xfabia13@stud.fit.vutbr.cz
+Čábela Radek    xcabel04@stud.fit.vutbr.cz
+Poposki Vasil   xpopos00@stud.fit.vutbr.cz
+Prosinec 2020, Fakulta informačních technologií VUT v Brně
+Generování kódu
+*/
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+void CodeGenStart();
+void CodeGenDefVar(char* id);
