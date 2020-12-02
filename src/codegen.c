9ff27b3f61eb79d61dcb3e610824019c7b6d1c26
VasilP202 <62769952+VasilP202@users.noreply.github.com>
2020-12-02 12:03:55 +0100

    Generovani stringu a floatu

--- src/codegen.c
+++ src/codegen.c
@@ -21,6 +21,65 @@ void CodeGenDefVar(char* id)
     printf("DEFVAR @LF %s\n",id);
 }
 
+char *gen_float(char *input_float) 
+{
+    double ret = strtod(input_float, NULL);
+    char *output_float = (char *) malloc(100);
+    sprintf(output_float, "float@%a", ret);
+
+    printf("%s\n", strlen(output_float));
+    return output_float;
+}
+
+char *generate_string(char* input_string) 
+{
+    
+    int len = strlen(input_string);
+    char buffer[5]; 
+    
+    char *output_string = malloc(len*3 + 7);
+    strcpy(output_string, "string@");
+    
+    int i, k = 7;
+
+    for(i = 0; i < len; i++)
+    {
+        if(input_string[i] == '"') {
+            
+            strcat(output_string, "\\034");
+            k += 3;
+            
+        } else if(input_string[i] == '#') {
+            
+            strcat(output_string, "\\035");
+            k += 3;
+            
+        } else if(input_string[i] == '\\') {
+            
+            strcat(output_string, "\\092");
+            k += 3;
+
+        } else if(input_string[i] <= 32) {
+            
+            sprintf(buffer, "\\%03d", input_string[i]);
+            
+            strcat(output_string, buffer);
+            k += 3;
+            
+        } else {
+            
+            output_string[k] = input_string[i];
+            
+        }
+
+        k++; 
+    }
+    
+    output_string[k] = '\0';
+
+    return output_string;
+}
+
 void CodeGenPrint(tToken *token)
 {
     if((*token)->type==T_INT)
