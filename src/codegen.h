088deee504a85b81d86e6ed4cfa7306ee48deb87
quapik <73609809+quapik@users.noreply.github.com>
2020-12-09 16:40:42 +0100

    add doxygen

--- src/codegen.h
+++ src/codegen.h
@@ -6,8 +6,9 @@ Fabián Michal   xfabia13@stud.fit.vutbr.cz
 Čábela Radek    xcabel04@stud.fit.vutbr.cz
 Poposki Vasil   xpopos00@stud.fit.vutbr.cz
 Prosinec 2020, Fakulta informačních technologií VUT v Brně
-Generování kódu
+Generování kódu, na souboru pracovali Šíma Vojtěch, Poposki Vasil
 */
+
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
@@ -18,13 +19,48 @@ Generování kódu
 #include "symtable.h."
 
 void CodeGenStart();
+/**
+ *
+ * @param id Jméno proměnné kterou budeme definovat
+ */
 void CodeGenDefVar(char* id);
+/**
+ *
+ * @param input_float Hodnota floatu ze scanneru která se převede na správný tvar pro výsledný kód
+ * @return Výsledná podoba floatu
+ */
 char *gen_float(char *input_float);
+/**
+ *
+ * @param input_string  Hodnota stringu ze scanneru která se převede na správný tvar pro výsledný kód
+ */
 void gen_string(char *input_string);
+/**
+ *
+ * @param token Ukazatel na aktuální token se kterým se bude dále ve funkci pracovat
+ */
 void CodeGenPrint(tToken *token);
+/**
+ * Funkce pro výpis vestavěné funkce substr
+ */
 void gen_substr();
+/**
+ * Funkce pro výpis vestavěné funkce inputi, inputs a inputf
+ *
+ */
 void gen_input();
+/**
+ * Funkce pro výpis vestavěné string funkce
+ */
 void gen_string_functions();
+/**
+ *
+ * @param token Ukazatel na aktuální token se kterým se bude dále ve funkci pracovat
+ * @param LocalTable Ukazatel na tabulku symbolů pro proměnné
+ * @param IDCounter Počítadlo pro informaci kolik bylo proměnných na levé straně
+ * @param UchovaniID Pomocná proměnná s informací o jménech proměnných
+ * @return Ukazatel na poslední token se kterým se pracovalo
+ */
 tToken CodeGenInputi(tToken *token, tSymbolTablePtrPromenna LocalTable,int IDCounter,char *UchovaniID[]);
 tToken CodeGenInputf(tToken *token, tSymbolTablePtrPromenna LocalTable,int IDCounter,char *UchovaniID[]);
 tToken CodeGenInputs(tToken *token, tSymbolTablePtrPromenna LocalTable,int IDCounter,char *UchovaniID[]);
